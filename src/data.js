8f7db07eaad1f30a1276b4938cef8390ff3149d7
PrussianJaeger <prussianjaeger@proton.me>
2025-05-10 23:43:59 -0500

    function added

--- src/data.js
+++ src/data.js
@@ -1,3 +1,26 @@
 function darkMode() {
 	document.querySelector("body").classList.toggle("dark-mode");
 }
+
+function template(img, name) {
+	return  `
+		<div class="box">
+			<div class="top">
+				<img src="${img}" alt="${name}">
+			</div>
+			<div class="bot">${name}</div>
+		</div>
+	`;
+}
+
+function showRecipes(recipes) {
+	const content = document.querySelector(".content");
+	let img, name;
+
+	for (recipe in recipes) {
+		img = recipe.img;
+		name = recipe.name;
+
+		content.appendChild(template(img, name));
+	}
+}
